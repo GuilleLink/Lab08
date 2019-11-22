ffa8db4410ec725e2886c02c1d862d7c011e7cee
GuilleLink <32148569+GuilleLink@users.noreply.github.com>
2019-11-22 01:31:01 -0600

    Cambio del build e implementacion de division. Cambio en el UI de la calculadora

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ class App extends Component{
   
   valueFather(valor){
     const value = valor.valor    
-    if((this.state.totalValue.length <=9 || (value==='+' || value==='-' || value==='*' || value==='=' || value==='C'|| value==='AC' || value==='%'))){    
+    if((this.state.totalValue.length <=9 || (value==='+' || value==='-' || value==='*' || value==='/' || value==='=' || value==='C'|| value==='AC' || value==='%'))){    
       if (value==='C'){
         this.setState({totalValue: '', error: false})
       }
@@ -81,6 +81,18 @@ class App extends Component{
       else if(value==='*' && this.state.firstNumber!=='' && this.state.float && !this.state.error){
         this.setState({totalValue: parseFloat(this.state.firstNumber) * parseFloat(this.state.totalValue), firstNumber: '', lastOperator: '*', float: false})
       }
+      else if(value==='/' && this.state.firstNumber==='' && !this.state.float && !this.state.error){
+        this.setState({firstNumber: this.state.totalValue, totalValue: '', lastOperator: '/', float: false})
+      }
+      else if(value==='/' && this.state.firstNumber!=='' && !this.state.float && !this.state.error){
+        this.setState({totalValue: parseInt(this.state.firstNumber) / parseInt(this.state.totalValue), firstNumber: '', lastOperator: '/', float: false})
+      }
+      else if(value==='/' && this.state.firstNumber==='' && this.state.float && !this.state.error){
+        this.setState({firstNumber: this.state.totalValue, totalValue: '', lastOperator: '/', float: false})
+      }
+      else if(value==='/' && this.state.firstNumber!=='' && this.state.float && !this.state.error){
+        this.setState({totalValue: parseFloat(this.state.firstNumber) / parseFloat(this.state.totalValue), firstNumber: '', lastOperator: '/', float: false})
+      }
       else if(value==='=' && this.state.lastOperator==='+' && !this.state.float && !this.state.error){
         this.setState({totalValue: parseInt(this.state.firstNumber) + parseInt(this.state.totalValue), firstNumber: '', lastOperator: '', float: false})
       }
@@ -90,6 +102,9 @@ class App extends Component{
       else if(value==='=' && this.state.lastOperator==='*' && !this.state.float && !this.state.error){
         this.setState({totalValue: parseInt(this.state.firstNumber) * parseInt(this.state.totalValue), firstNumber: '', lastOperator: '', float: false})
       }
+      else if(value==='=' && this.state.lastOperator==='/' && !this.state.float && !this.state.error){
+        this.setState({totalValue: parseInt(this.state.firstNumber) / parseInt(this.state.totalValue), firstNumber: '', lastOperator: '', float: false})
+      }
       else if(value==='=' && this.state.lastOperator==='+' && this.state.float && !this.state.error){
         this.setState({totalValue: parseFloat(this.state.firstNumber) + parseFloat(this.state.totalValue), firstNumber: '', lastOperator: '', float: false})
       }
@@ -98,6 +113,9 @@ class App extends Component{
       }
       else if(value==='=' && this.state.lastOperator==='*' && this.state.float && !this.state.error){
         this.setState({totalValue: parseFloat(this.state.firstNumber) * parseFloat(this.state.totalValue), firstNumber: '', lastOperator: '', float: false})
+      }
+      else if(value==='=' && this.state.lastOperator==='/' && this.state.float && !this.state.error){
+        this.setState({totalValue: parseFloat(this.state.firstNumber) / parseFloat(this.state.totalValue), firstNumber: '', lastOperator: '', float: false})
       }
       else if(value==='=' && this.state.lastOperator==='' && !this.state.error){
         this.setState({totalValue: this.state.totalValue, firstNumber: '', lastOperator: ''})
@@ -149,6 +167,7 @@ class App extends Component{
       <Boton id='num0' valor='0' click={this.valueFather} />      
       <Boton id='period' valor='.' click={this.valueFather} />      
       <Boton id='equals' valor='=' click={this.valueFather} />
+      <Boton id='division' valor='/' click={this.valueFather} />
     </div>)
   }
 }
